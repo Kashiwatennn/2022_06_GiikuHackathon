b21ac2931db95411c9a0716165582283fdf5a4a2
it-tsumugi <tsumugi.it@gmail.com>
2022-06-04 17:57:44 +0900

    ユーザーidを状態に追加

--- src/components/utility/SetInitialState.tsx
+++ src/components/utility/SetInitialState.tsx
@@ -1,9 +1,14 @@
 import { FC } from "react";
+import { auth } from "../../firebase";
 
 type propsType = {
   children: React.ReactNode;
 };
 
+const user = auth.currentUser;
+if (user !== null) {
+  const uid = user.uid;
+}
 export const SetInitialState: FC<propsType> = ({ children }) => {
   return <div>{children}</div>;
 };
