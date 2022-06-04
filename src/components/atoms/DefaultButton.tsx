b10dd0515a92372b0713b00c9a9b434181dd1e42
Kashiwatennn <karent@keio.jp>
2022-06-04 16:21:27 +0900

    ボタンの仮修正

--- src/components/atoms/DefaultButton.tsx
+++ src/components/atoms/DefaultButton.tsx
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-export const DefaultButton = styled.button``;
+export const DefaultButton = styled.button`
+
+    /*button*/
+    border: none;
+    display:block;
+    text-decoration: none;
+    background:#f1c43b;
+    text-align:center;
+    color:#FFFFFF;
+    font-size:12px;
+    font-weight:bold;
+    border-radius:26px;
+    -webkit-border-radius:26px;
+    -moz-border-radius:26px;
+    ;
+
+    /*button hover*/
+    &:hover{
+    background:#ddb91a;
+    color:#FFFFFF;
+    margin-left:3px;
+    margin-top:3px;
+    box-shadow:none;
+    }
+`;
+
+    
